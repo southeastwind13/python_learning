09b7509ebf52c255d30ee462a095ee28b237ca03
Watcharapong Wongrattanasirikul <watcharapongwongrattanasirikul@Watcharapongs-MacBook-Pro.local>
2024-06-08 00:23:29 +0700

    Add bin function

---
+++ function/bin.py
@@ -0,0 +1,18 @@
+'''
+bin
+
+Function to convert a number to binary.
+
+Syntax:
+bin(number)
+
+Parameters:
+number: The number to convert to binary.
+
+Returns:
+Binary representation of the number.
+'''
+
+number = 15
+
+print(f'The binary equivalent of 15 is {bin(number)}')
