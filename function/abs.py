3292ce74f9daafccc54ea152db854d6898c4b96e
Watcharapong Wongrattanasirikul <watcharapongwongrattanasirikul@Watcharapongs-MacBook-Pro.local>
2024-06-08 00:06:42 +0700

    Add absolute function

---
+++ function/abs.py
@@ -0,0 +1,16 @@
+'''
+abs (Absolute)
+
+Function to calculate magnitude of a number. 
+
+:param number: The number to calculate magnitude (Can be real or complex).
+:return: Absolute value of the number (Can be int or float).
+'''
+
+intger_number = -10
+floating_number = -3.14
+complex_number   = 3 + 4j
+
+print(abs(intger_number))
+print(abs(floating_number))
+print(abs(complex_number))
