ff480c2636f0f01a1bee4415d6d9f8e810ef3ccb
Watcharapong Wongrattanasirikul <sewind@WatcharpongsMBP.home>
2024-08-10 11:40:19 -0700

    update comment for single responsibility

--- solid/single_responsibility_principle.py
+++ solid/single_responsibility_principle.py
@@ -1,12 +1,16 @@
 '''
 Concept: A class should have only one reason to change, meaning it should have only one job or responsibility.
 
-Explanation: Do only one job e.g. If a class do processing, it should do only porcessing, not handle data storage, logging, error handling etc.
+Real world situation: Chef needs to only cook, not do the ordering, billing, etc.
+
+In Practice, A class do only one job. e.g. If a class do processing, it should do only porcessing, not handle data storage, logging, error handling etc.
+
+Example: If we have a class to process multiple jobs, then it should be split for each job.
 '''
 
 
 '''
-Example CostProcessing violates the single responsibility principle becuase the class do both processing and saving the cost.
+CostProcessing calss violates the single responsibility principle becuase the class do both processing and saving the cost.
 '''
 class CostProcessing:
     def __init__(self, cost) -> None:
